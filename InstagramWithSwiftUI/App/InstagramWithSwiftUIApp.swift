62874f3966f168bcfccf6456d46f2c0e0bf18695
JeongDeokho <ekdj98@gmail.com>
2024-04-12 10:29:37 +0900

    Initial Commit
    Update README.md
    
    Create README.md
    
    InitCommit

---
+++ InstagramWithSwiftUI/App/InstagramWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramWithSwiftUIApp.swift
+//  InstagramWithSwiftUI
+//
+//  Created by Jeong Deokho on 2024/04/12.
+//
+
+import SwiftUI
+
+@main
+struct InstagramWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# 인스타그램 클론 With SwiftUI 
+https://www.udemy.com/course/swiftui-instagram/
+해당 강의를 듣고 인스타그램 클론앱을 만들어보는 프로젝트입니다. 
